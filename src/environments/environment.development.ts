f5f40a03cdbe2907aa29abc04e3e076dd2193cd4
OmarMSS88 <91123631+OmarMSS88@users.noreply.github.com>
2024-08-07 14:15:53 +0200

    adjustments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false, // this is true in the production environment
   AUTH0_DOMAIN: "dev-omar.eu.auth0.com",
   AUTH0_CLIENT_ID: "IJrmgYbTmQRyOQfx6lenbpduRJzfOPRh",
-  redirectUri: "http://localhost:4200",
+  redirectUri: "https://66afa1a30e9243be3def3e50--mercenaries.netlify.app/",
   api_url: "https://mercenaries.azurewebsites.net",
   AUTH0_AUDIENCE: "https://mercenaries.azurewebsites.net/",
-  home_url: "http://localhost:4200"
+  home_url: "https://66afa1a30e9243be3def3e50--mercenaries.netlify.app/"
 };

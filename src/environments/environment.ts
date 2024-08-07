c3838db9323a8270763e0a26253a2f7647c80f32
OmarMSS88 <91123631+OmarMSS88@users.noreply.github.com>
2024-08-07 14:24:47 +0200

    adjusted callback

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: false, // this is true in the production environment
-    AUTH0_DOMAIN: "dev-omar.eu.auth0.com",
-    AUTH0_CLIENT_ID: "IJrmgYbTmQRyOQfx6lenbpduRJzfOPRh",
-    redirectUri: "https://66afa1a30e9243be3def3e50--mercenaries.netlify.app/",
-    api_url: "https://mercenaries.azurewebsites.net",
-    AUTH0_AUDIENCE: "https://mercenaries.azurewebsites.net/",
-    home_url: "https://66afa1a30e9243be3def3e50--mercenaries.netlify.app/"
-  };
+  production: false, // this is true in the production environment
+  AUTH0_DOMAIN: "dev-omar.eu.auth0.com",
+  AUTH0_CLIENT_ID: "IJrmgYbTmQRyOQfx6lenbpduRJzfOPRh",
+  redirectUri: "https://mercenaries.netlify.app/",
+  api_url: "https://mercenaries.azurewebsites.net",
+  AUTH0_AUDIENCE: "https://mercenaries.azurewebsites.net/",
+  home_url: "https://mercenaries.netlify.app/"
+};
